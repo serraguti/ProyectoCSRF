42a6f2f1bb39ff0f4105771ae2ba1046c2df47f5
serraguti <pacoserranox@gmail.com>
2022-01-24 14:25:21 +0100

    Add project files.

---
+++ ProyectoCSRF/Controllers/ManagedController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoCSRF.Controllers
+{
+    public class ManagedController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string usuario, string password)
+        {
+            if (usuario.ToUpper() == "ADMIN"
+                && password.ToUpper() == "ADMIN")
+            {
+                HttpContext.Session.SetString("USUARIO", usuario);
+                return RedirectToAction("Productos", "Tienda");
+            }else
+            {
+                ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+                return View();
+            }
+        }
+
+        public IActionResult AccesoDenegado()
+        {
+            return View();
+        }
+    }
+}
