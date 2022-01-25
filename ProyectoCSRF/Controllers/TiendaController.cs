b7291ba56b23b6e21bae1388e51e83315834e04d
serraguti <pacoserranox@gmail.com>
2022-01-25 10:07:27 +0100

    commit

--- ProyectoCSRF/Controllers/TiendaController.cs
+++ ProyectoCSRF/Controllers/TiendaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,38 @@ namespace ProyectoCSRF.Controllers
 {
     public class TiendaController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Productos()
         {
-            return View();
+            if (HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("AccesoDenegado", "Managed");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Productos(string direccion, string[] producto)
+        {
+            if (HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("AccesoDenegado", "Managed");
+            }
+            //NECESITAMOS ENVIAR INFORMACION DESDE
+            //PRODUCTOS HASTA CARRITO
+            TempData["PRODUCTOS"] = producto;
+            TempData["DIRECCION"] = direccion;
+            return RedirectToAction("Carrito");
+        }
+
+        public IActionResult Carrito()
+        {
+            string[] productos = TempData["PRODUCTOS"] as string[];
+            string direccion = TempData["DIRECCION"].ToString();
+            ViewBag.Direccion = direccion;
+            return View(productos);
         }
     }
 }
